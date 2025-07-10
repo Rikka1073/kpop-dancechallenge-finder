b85d3e3646121fde8d1ae4f19b2a5e1d2cb41418
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-07-10 22:39:20 +0900

    style:コードフォーマットの実施

--- src/libs/supabaseFunction.ts
+++ src/libs/supabaseFunction.ts
@@ -2,9 +2,7 @@ import { supabase } from "./supabase";
 
 // 全ての動画を取得（初期表示用）
 export const getAllVideos = async () => {
-  const { data, error } = await supabase
-    .from("videos")
-    .select(`*, video_groups(groups(id, group_name)), video_songs(songs(id, song_name))`);
+  const { data, error } = await supabase.from("videos").select(`*, video_groups(groups(id, group_name)), video_songs(songs(id, song_name))`);
 
   if (error) {
     console.log("Error fetching videos:", error);
