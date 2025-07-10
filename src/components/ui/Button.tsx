03ef9c8c7b1785e06735665f8f420a6aea226b1b
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-07-10 23:49:14 +0900

    update:クラスのpropsの追加

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,10 +1,10 @@
 import { ButtonProps } from "@/types";
 
-const Button = ({ id, name, text }: ButtonProps) => {
+const Button = ({ id, name, text, onClick, className }: ButtonProps) => {
   return (
     <div>
-      <button id={id} name={name} className="btn btn-neutral btn-outline rounded-2xl">
-        {text}
+      <button id={id} name={name} onClick={onClick} className={className ? className : "btn btn-neutral btn-outline rounded-2xl"}>
+        {name === "clear" ? "おはよう" + text : text}
       </button>
     </div>
   );
