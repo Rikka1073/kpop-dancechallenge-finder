e36b72c73656045c8f2592cc218c0ec8d90089a3
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-07-10 23:18:31 +0900

    add:グループデータを取得してボタンを作成

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,8 +6,8 @@ import { fetchGroups, fetchSongs, getAllVideos } from "@/libs/supabaseFunction";
 import useSWR from "swr";
 
 const Search = () => {
-  const { data: songs, error: songsError, isLoading: songsLoading } = useSWR("songs", fetchSongs);
   const { data: videos, error: videosError, isLoading: videosLoading } = useSWR("videos", getAllVideos);
+  const { data: songs, error: songsError, isLoading: songsLoading } = useSWR("songs", fetchSongs);
   const { data: groups, error: groupsError, isLoading: groupsLoading } = useSWR("groups", fetchGroups);
   const isLoading = songsLoading || videosLoading || groupsLoading;
   const isError = songsError || videosError || groupsError;
@@ -22,9 +22,17 @@ const Search = () => {
           ダンスチャレンジ検索
         </h2>
         <div className="text-center mb-6">お気に入りの楽曲やグループを選んで検索しよう！</div>
+
         <div className="mb-8">
           <h3 className="text-2xl font-bold mb-3">楽曲で検索</h3>
           <div className="flex flex-wrap gap-2">{songs && songs.map((item) => <Button key={item.id} id={item.id} name="songs" text={item.song_name} />)}</div>
+        </div>
+
+        <div className="mb-8">
+          <h3 className="text-2xl font-bold mb-3">グループで検索</h3>
+          <div className="flex flex-wrap gap-2">
+            <div className="flex flex-wrap gap-2">{groups && groups.map((item) => <Button key={item.id} id={item.id} name="songs" text={item.group_name} />)}</div>
+          </div>
         </div>
       </Layout>
     </div>
