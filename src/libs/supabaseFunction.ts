4eedab87be302c34a02a1cbbf2d464615eb2e948
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-07-10 22:29:14 +0900

    add:曲とグループの情報を取得する関数の作成

--- src/libs/supabaseFunction.ts
+++ src/libs/supabaseFunction.ts
@@ -9,7 +9,27 @@ export const getAllVideos = async () => {
   if (error) {
     console.log("Error fetching videos:", error);
   } else if (data) {
-    // console.log("Videos fetched successfully:", data);
+    console.log("Videos fetched successfully:", data);
     return data || [];
+  }
+};
+
+// 曲の情報を取得
+export const fetchSongs = async () => {
+  const { data, error } = await supabase.from("songs").select("*");
+  if (error) {
+    console.log("Error fetching songs:", error);
+  } else if (data) {
+    return data;
+  }
+};
+
+// グループの情報を取得
+export const fetchGroups = async () => {
+  const { data, error } = await supabase.from("groups").select("*");
+  if (error) {
+    console.log("Error fetching Groups:", error);
+  } else if (data) {
+    return data;
   }
 };
