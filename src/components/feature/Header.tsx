787c137173e8acdd0dab54127cf68fd4870b6bce
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-07-19 21:15:47 +0900

    style:btnからlinkに変更

--- src/components/feature/Header.tsx
+++ src/components/feature/Header.tsx
@@ -5,20 +5,20 @@ const Header = () => {
   return (
     <div className="navbar shadow-sm">
       <div className="flex-1">
-        <Link href="/" className="btn bg-white text-xl text-black">
-          daisyUI
+        <Link href="/" className="link text-xl text-black no-underline">
+          K-POP Challenge
         </Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal hidden gap-2 px-1 sm:flex">
           <li>
-            <Link href="/" className="btn border-none bg-white shadow-none hover:text-purple-400">
+            <Link href="/" className="link no-underline hover:bg-transparent hover:text-purple-400">
               <House />
               ホーム
             </Link>
           </li>
           <li>
-            <Link href="/search" className="btn border-none bg-white shadow-none hover:text-purple-400">
+            <Link href="/search" className="link no-underline hover:bg-transparent hover:text-purple-400">
               <Search />
               検索
             </Link>
@@ -26,7 +26,7 @@ const Header = () => {
           <li>
             <Link
               href="/search"
-              className="btn transform rounded-2xl bg-gradient-to-r from-purple-600 to-pink-600 text-white shadow-xl transition-all duration-200 hover:scale-105 hover:shadow-2xl"
+              className="link transform rounded-2xl bg-gradient-to-r from-purple-600 to-pink-600 text-white no-underline shadow-xl transition-all duration-200 hover:scale-105 hover:shadow-2xl"
             >
               <User />
               ログイン
