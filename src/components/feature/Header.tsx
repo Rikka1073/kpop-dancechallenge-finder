4d7a8c9b76dc7c8dd5d3baa70975eac47d860e39
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-08-20 23:28:13 +0900

    add:管理者用のリンク追加

--- src/components/feature/Header.tsx
+++ src/components/feature/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-import { CirclePlus, House, Search, User } from "lucide-react";
+import { Protect, SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { CirclePlus, House, Search, ShieldUser } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -10,9 +10,11 @@ const Header = () => {
 
   return (
     <div className="navbar w-full justify-between px-4 shadow-sm">
-      <Link href="/" className="link text-xl text-black no-underline">
-        K-POP Challenge
-      </Link>
+      <div>
+        <Link href="/" className="link text-xl text-black no-underline">
+          K-POP Challenge
+        </Link>
+      </div>
       <div className="md:flex md:w-fit md:items-center md:justify-end md:gap-4">
         <div className="fixed bottom-3 left-0 z-50 w-full px-4 md:static md:w-fit md:px-0">
           <div className="rounded-2xl bg-white shadow-lg md:bg-transparent md:shadow-none">
@@ -35,20 +37,34 @@ const Header = () => {
                   動画登録
                 </Link>
               </li>
+              <Protect role="org:admin">
+                <li className="hidden md:block">
+                  <Link href="/admin" className="link no-underline hover:bg-transparent hover:text-purple-400">
+                    <ShieldUser />
+                    管理
+                  </Link>
+                </li>
+              </Protect>
             </ul>
           </div>
         </div>
-        <SignedOut>
-          <SignInButton />
-          <SignUpButton>
-            <button className="link transform rounded-2xl bg-gradient-to-r from-purple-600 to-pink-600 px-4 py-2 font-bold text-white no-underline shadow-xl transition-all duration-200 hover:scale-105 hover:shadow-2xl">
-              Sign Up
-            </button>
-          </SignUpButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+        <div className="flex gap-2">
+          <SignedOut>
+            <SignUpButton>
+              <button className="link transform rounded-2xl bg-gradient-to-r from-purple-600 to-pink-600 px-2 py-2 text-sm font-bold text-white no-underline shadow-xl transition-all duration-200 hover:scale-105 hover:shadow-2xl">
+                新規登録
+              </button>
+            </SignUpButton>
+            <SignInButton>
+              <button className="link transform rounded-2xl bg-gradient-to-r from-purple-600 to-pink-600 px-2 py-2 text-sm font-bold text-white no-underline shadow-xl transition-all duration-200 hover:scale-105 hover:shadow-2xl">
+                ログイン
+              </button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
       </div>
     </div>
   );
