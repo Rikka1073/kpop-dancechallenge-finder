5b8395a0d0ea025256abf448bc14c263d1e92fc2
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-08-24 18:15:54 +0900

    add:サインインとサインアウトのモック追加

--- setupTests.tsx
+++ setupTests.tsx
@@ -4,6 +4,8 @@ vi.mock("@clerk/nextjs", () => {
   return {
     ClerkProvider: ({ children }: { children: React.ReactNode }) => <>{children}</>,
     Protect: ({ children }: { children: React.ReactNode }) => <>{children}</>,
+    SignedIn: ({ children }: { children: React.ReactNode }) => <>{children}</>,
+    SignedOut: ({ children }: { children: React.ReactNode }) => <>{children}</>,
     useUser: () => ({
       isSignedIn: true,
       user: { id: "user_123", fullName: "Test User" },
