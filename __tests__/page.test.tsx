affcaa863a7956132e5d1769b3600cbf7279d2cc
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-08-24 17:13:08 +0900

    update:テストにclerkを追加

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,8 +1,13 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Home from "@/app/page";
+import { ClerkProvider } from "@clerk/nextjs";
 
 test("Home", () => {
-  render(<Home />);
+  render(
+    <ClerkProvider>
+      <Home />
+    </ClerkProvider>
+  );
   expect(screen.getByTestId("home-title"));
 });
