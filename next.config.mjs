19776f47657982b33ae044e1d2bf6b023030a500
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-08-24 22:01:35 +0900

    add:webpackの設定追加

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,17 @@ const nextConfig = {
   images: {
     domains: ["i.ytimg.com"],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        path: require.resolve("path-browserify"),
+        fs: false,
+        os: false,
+      };
+    }
+    return config;
+  },
 };
 
 if (process.env.NODE_ENV === "development") {
