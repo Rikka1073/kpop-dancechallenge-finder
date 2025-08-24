011b516135b637749ed01db85918de40a659a02f
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-08-24 19:39:34 +0900

    update:buildエラーの解消

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,17 @@ const nextConfig = {
   images: {
     domains: ["i.ytimg.com"],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // クライアントサイドでは fs を空オブジェクトに置き換え
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+      };
+    }
+    return config;
+  },
 };
 
 if (process.env.NODE_ENV === "development") {
