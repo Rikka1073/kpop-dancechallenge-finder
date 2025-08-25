8a1f780e9d1b603f6c610c99a433c198dc8dc7bc
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-08-25 22:10:36 +0900

    add:ログインしてからメニューを出すように変更

--- src/components/feature/Header.tsx
+++ src/components/feature/Header.tsx
@@ -19,31 +19,33 @@ const Header = () => {
         <div className="fixed bottom-3 left-0 z-50 w-full px-4 md:static md:w-fit md:px-0">
           <div className="rounded-2xl bg-white shadow-lg md:bg-transparent md:shadow-none">
             <ul className="menu menu-horizontal flex w-full justify-center gap-2 px-1 md:flex-none md:justify-end md:gap-4">
-              <li>
-                <Link href="/" className="link no-underline hover:bg-transparent hover:text-purple-400">
-                  <House />
-                  ホーム
-                </Link>
-              </li>
-              <li>
-                <Link href="/search" className="link no-underline hover:bg-transparent hover:text-purple-400">
-                  <Search />
-                  検索
-                </Link>
-              </li>
-              <li>
-                <Link href="/register" className="link no-underline hover:bg-transparent hover:text-purple-400">
-                  <CirclePlus />
-                  動画登録
-                </Link>
-              </li>
-              <Protect role="org:admin">
-                <li className="hidden md:block">
-                  <Link href="/admin" className="link no-underline hover:bg-transparent hover:text-purple-400">
-                    <ShieldUser />
-                    管理
+              <Protect>
+                <li>
+                  <Link href="/" className="link no-underline hover:bg-transparent hover:text-purple-400">
+                    <House />
+                    ホーム
                   </Link>
                 </li>
+                <li>
+                  <Link href="/search" className="link no-underline hover:bg-transparent hover:text-purple-400">
+                    <Search />
+                    検索
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/register" className="link no-underline hover:bg-transparent hover:text-purple-400">
+                    <CirclePlus />
+                    動画登録
+                  </Link>
+                </li>
+                <Protect role="org:admin">
+                  <li className="hidden md:block">
+                    <Link href="/admin" className="link no-underline hover:bg-transparent hover:text-purple-400">
+                      <ShieldUser />
+                      管理
+                    </Link>
+                  </li>
+                </Protect>
               </Protect>
             </ul>
           </div>
